349d15981feb5ec8b40df20994ee700bb434aa22
LiuHuankai <39264858+LiuHuankai@users.noreply.github.com>
2018-06-28 07:57:39 +0800

    new

--- Classes/EndScene/EndScene.cpp
+++ Classes/EndScene/EndScene.cpp
@@ -28,33 +28,34 @@ bool EndScene::init()
 	Vec2 orgin = Director::getInstance()->getVisibleOrigin();
 	auto*pBackgroud = Sprite::create("Backgroud.png");
 	pBackgroud->setPosition(size.width / 2, size.height / 2);
-	
-	this->addChild(pBackgroud);
-	auto lable = Label::create("YOU DIED! ", "Arial", 36);
-	lable->enableGlow(Color4B::RED);
-	lable->setPosition(320, 240);
+	this->addChild(pBackgroud,0);
+	auto lable = Label::create("YOU DIED! ", "Arial", 60);
+	lable->setTextColor(Color4B::RED);
+	lable->setPosition(size.width / 2, size.height / 2+160);
 	addChild(lable);
 	auto lable2 = Label::create("RANK ", "Arial", 36);
 	lable2->setTextColor(Color4B::RED);
-	lable2->setPosition(160, 180);
+	lable2->setPosition(size.width / 2-280, size.height / 2+60);
 	addChild(lable2);
 	auto lable3 = Label::create("MAXSIZE ", "Arial", 36);
 	lable3->setTextColor(Color4B::BLACK);
-	lable3->setPosition(160, 120);
+	lable3->setPosition(size.width / 2 - 280, size.height / 2-40);
 	addChild(lable3);
 
 	auto pMenuRETRY = MenuItemImage::create("button.png", "button_a.png", this, menu_selector(EndScene::MenuButtonRetry));
+	pMenuRETRY->setScale(1.5);
 	auto labelRetry = LabelTTF::create("RETRY", "Arial", 18);
 	auto pLabeRetry = MenuItemLabel::create(labelRetry);
 	auto menuRetry = Menu::create(pMenuRETRY, pLabeRetry, NULL);
-	menuRetry->setPosition(size.width / 2, size.height / 2 - 60);
+	menuRetry->setPosition(size.width / 2, size.height / 2 +60);
 	this->addChild(menuRetry);
 
 	auto pMenuExit = MenuItemImage::create("button.png", "button_a.png", this, menu_selector(EndScene::MenuButtonExit));
+	pMenuExit->setScale(1.5);
 	auto labelExit = LabelTTF::create("EXIT", "Arial", 18);
 	auto pLabelExit = MenuItemLabel::create(labelExit);
 	auto menuExit = Menu::create(pMenuExit, pLabelExit, NULL);
-	menuExit->setPosition(size.width / 2, size.height / 2 - 120);
+	menuExit->setPosition(size.width / 2, size.height / 2 - 40);
 	this->addChild(menuExit);
 	return true;
 }
